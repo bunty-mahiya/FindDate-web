ee60620207b30d87fa868d27373c77abbbdd9f1d
bunty <buntymahiya764@gamil.com>
2026-04-24 18:38:54 +0530

    localHost

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,6 +2,11 @@ import io from "socket.io-client"
 import { BASE_URL } from "./constants"
 
 export const connectionSocket = ()=>{
-    return io(BASE_URL)  // connect backend server to frontend server
+ if(location.hostname == "localhost"){
+   return io(BASE_URL)    // connect backend server to frontend server
+ }else{
+    return io("/" ,{path:"/api/socket.io"})
+ }
+     
 }
 
